8b13fbd11281a0ed45fd03d46b3646b5837b2e29
NguyenHien <hien2k6tta@gmail.com>
2026-01-15 21:28:39 +0700

    feat/login

---
+++ src/main/java/org/example/model/UserAccount.java
@@ -0,0 +1,10 @@
+package org.example.model;
+
+public class UserModel {
+    private int id;
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private  String
+}
